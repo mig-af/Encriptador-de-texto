6d45efc54b5623d302d8b58fcac9e7e034d55fbe
mig-af <mkmouse786@gmail.com>
2024-08-16 23:06:12 +0000

    css responsive hecho, funcion encriptar creada

--- scripts/app.js
+++ scripts/app.js
@@ -1,93 +1,123 @@
 
-
-let word = "cobermufatnimescaicimesobern";
-
 let letrasParaDesencriptar = ["ai", "enter", "imes", "ober", "ufat"];
 let vocales = ["a", "e", "i", "o", "u"];
-
-
-
-
-
-
-
-
-
+let caracteresNoPermitidos = /[A-Z á-ú à-ù Ñ !@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]/g
+let primeraEncriptacion = false;
 
 
 function desencriptarPalabra(palabrita){
-
-    //inicializamos la variable desencriptado para hacer un mejor control del bucle while
     let desencriptado = false;
-    //contador para acceder a los indices de la lista 
+    //esta variable es para acceder a los indices de la lista letrasPara desencriptar
     let cont=0
-
-    //variable de seguridad :) NO interviene en el proceso de desencriptacion
+    let palabraDesencriptada = palabrita;
+    //Iniciamos esta variable para controlar la veces que se analizo la palabra para buscar la letra a desencriptar
+    //Debe ser menor a la cantidad de palabras a desencriptar, si es mayor significa que ya no hay mas palabras para desencriptar
+    let controlador = 0;
+    //esta variable es para cortar el bucle si ocurre algun error
     let cortarBucle = 0;
 
-    //guardamos la palabra que debemos desencriptar 
-    let palabraDesencriptada = palabrita;
-
-    //Controlador para guardar el numero de intentos de desencriptacion que se hizo
-    let controlador = 0;
-    
-    //Iniciamos el bucle
+    //Analizamos cada letra de la palabra
     while(desencriptado != true){
         
-        //Verificamos 
+        //verificamos la variable cont, para acceder a los indices de la lista lestrasDesencriptar
         if(cont > letrasParaDesencriptar.length-1){cont=0}
         
-        //buscamos las coincidencias de las letras que queremos desencriptar en la palabra que nos dio el usuario
+        //buscamos las coincidencias
         if(palabraDesencriptada.match(letrasParaDesencriptar[cont]) != null){
-            //reemplazamos
             palabraDesencriptada = palabraDesencriptada.replace(letrasParaDesencriptar[cont], vocales[cont]);
             
-            //volvemos a controlador a 0 por que aun hay palabras para desencriptar
             controlador = 0;
         }else{
-            //Cada vez que no se realiza la desencriptacion
-            //este contador sube, si llega a +5(cantidad de palabras que tenemos) significa
-            //que se trato de desencriptar todas las palabras y ya no se encontraron coincidencias
         
             controlador ++;
-            
-            //Si controlador es mayor que el numero de palabras("enter", "imes", "ober " etc)
-            //cortamos el bucle y damos por finalizado todo por que se encontraron todas las palabras
+            //verificamos si ya se analizaron todas las coincidencias
             if(controlador > letrasParaDesencriptar.length){
                 //console.log(palabraDesencriptada);
-                
                 desencriptado = true;
-                
             }
         }  
         cont ++;
         cortarBucle ++;
-
-        //para analizar una palabra solo debe haber maximo 20 vueltas
-        //Por eso Si ocurre algun tipo de bug, el bucle se rompera en la vuelta 100
-        //Para no afectar al usuario y su sistema
         
         if(cortarBucle > 100){
+            palabraDesencriptada = "Hubo un error no se logro desencriptar el texto";
             break;
         }
 
     }
     return palabraDesencriptada;
-
 }
 
-let ja = desencriptarPalabra("Lai cobermufatnimAescaicimesobern foberrenternsenter baisenters coberncenterptufatailenters denter lai cobermufatnimescaicimesobern");
 
-
-let caracteresNoPermitidos = /[A-Z á-ú à-ù Ñ !@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]/g
-let siu = `[A-Z á-ú à-ù Ñ !@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]`.split("");
-function verificarCaracteres(texto){
-    //Verificamos si hay caracteres especiales, si es true, devolvera una lista con un booleano y la letra que se encontro
-    //si no se encuentra nada, devolvera un false
-    let existenCaracteres = (texto.match(caracteresNoPermitidos) != null)? [true, texto.match(caracteresNoPermitidos)] : false 
+function verificarCaracteres(texto, caracteres){
+    let existenCaracteres = (texto.match(caracteresNoPermitidos) != null)? [true, texto.match(caracteres)] : false 
     return existenCaracteres;
 }
-console.log(verificarCaracteres("holaaks"));
+
+
+function encriptarFrase(texto){
+    let textoEncriptado = "";
+    
+    //recorremos el texto 
+    for(let i=0; i<texto.length; i++){
+        //definimos una variable para guardar las letras que no son vocales
+        let letra = "";
+        //Por cada letra en el texto, analizamos si coincide con cada vocal dentro la lista
+        for( let j=0; j<vocales.length; j++){
+            
+            //si la letra coincide con una vocal dentro la lista, se hace el reemplazo por las letras a encriptar
+            if(texto[i] === vocales[j] ){
+                textoEncriptado += letrasParaDesencriptar[j];
+            //si la letra no es una vocal que esta dentro la lista vocales, se guarda en la variable letra
+            }else if(vocales.includes(texto[i]) == false){
+                letra = texto[i];
+            }
+            
+        }
+        //incluimos la letra que no es una vocal en la variable textoEncriptado
+        textoEncriptado += letra;
+    }
+    //retornamos todo el texto encriptado
+    return textoEncriptado;
+
+}
+
+
+function encriptar(){
+
+    if(primeraEncriptacion == true){
+        let textoUsuario = document.getElementById("texto").value;
+        document.getElementById("textoEncriptado").innerHTML=textoUsuario;
+        
+        return;
+    }else{
+        //console.log(encriptado);
+        cambiarEstiloElemento(".encriptador__resultado__texto__encriptado", "display", "flex");
+        //document.querySelector(".encriptador__resultado__texto__encriptado").style.display="flex";
+        eliminarElemento(".encriptador__resultado__imagen");
+        eliminarElemento(".encriptador__resultado__texto");
+        
+        let text = document.getElementById("texto").value;
+        //cambiarEstiloElemento(".encriptador__resultado__texto__encriptado", "display", "flex");
+        //document.querySelector(".encriptador__resultado__texto__encriptado").style.display="flex";
+        document.getElementById("textoEncriptado").innerHTML=text;
+        primeraEncriptacion = true;
+        return;
+    }
+}
+
+
+
+function eliminarElemento(nombreClaseId){
+    document.querySelector(nombreClaseId).remove();
+    return;
+}
+function cambiarEstiloElemento(elemento,propieda, valor){
+    window["document"]["querySelector"](elemento)["style"][propieda]=valor;
+    //window.document.querySelector(elemento).style.backgroundColor=valor;
+    return;
+}
+
 
 
 

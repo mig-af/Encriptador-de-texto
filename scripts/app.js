3d7b627075fefc7f402ba779cd8af4b10502ae69
mig-af <mkmouse786@gmail.com>
2024-08-22 20:49:56 +0000

    terminado

--- scripts/app.js
+++ scripts/app.js
@@ -1,67 +1,30 @@
 
 let letrasParaDesencriptar = [/ai/g, /enter/g, /imes/g, /ober/g, /ufat/g];
 let vocales = ["a", "e", "i", "o", "u"];
-let caracteresNoPermitidos = /[A-Zá-úà-ùÑ!@#$%^&*)(_+\-=\[\]{};:"\\|,.<>\/?]/g
+let caracteresNoPermitidos = /[A-Zá-úà-ùÑ!@#$%^&*)(_+\-=\[\]{};:"\\|,.<>\/?'|]/g
 let primeraEncriptacion = true;
 
+//colores
+let colorError = "#0A3871";
+let colorAcertar = "green";
 
 
-function desencriptarPalabra(palabrita){
-    let desencriptado = false;
-    //esta variable es para acceder a los indices de la lista letrasPara desencriptar
-    let cont=0
-    let palabraDesencriptada = palabrita;
-    //Iniciamos esta variable para controlar la veces que se analizo la palabra para buscar la letra a desencriptar
-    //Debe ser menor a la cantidad de palabras a desencriptar, si es mayor significa que ya no hay mas palabras para desencriptar
-    let controlador = 0;
-    //esta variable es para cortar el bucle si ocurre algun error
-    let cortarBucle = 0;
 
-    //
-    let coincidencias = 0;
-
-    //Analizamos cada letra de la palabra
-    while(desencriptado != true){
-        
-        //verificamos la variable cont, para acceder a los indices de la lista lestrasDesencriptar
-        if(cont > letrasParaDesencriptar.length-1){cont=0}
-        
-        //buscamos las coincidencias
-        if(palabraDesencriptada.match(letrasParaDesencriptar[cont]) != null){
-            palabraDesencriptada = palabraDesencriptada.replace(letrasParaDesencriptar[cont], vocales[cont]);
-            coincidencias ++;
-            controlador = 0;
-        }else{
-        
-            controlador ++;
-            
-            if(coincidencias < 5){
-                desencriptado = true;
-
-            }
-            //verificamos si ya se analizaron todas las coincidencias
-            if(controlador > letrasParaDesencriptar.length){
-                //console.log(palabraDesencriptada);
-                
-                desencriptado = true;
-            }
-        }  
-        cont ++;
-        cortarBucle ++;
-        
-        if(cortarBucle > 100){
-            palabraDesencriptada = "Hubo un error no se logro desencriptar el texto";
-            break;
-        }
-
+function desencriptarTexto(texto){
+    let textoDesencriptado = texto;
+    
+    for(let i=0; i<letrasParaDesencriptar.length; i++){
+        textoDesencriptado = textoDesencriptado.replace(letrasParaDesencriptar[i], vocales[i]);
     }
-    return palabraDesencriptada;
+    return textoDesencriptado;
 }
 
 
-function encriptarFrase(texto){
+function encriptarTexto(texto){
     let textoEncriptado = "";
-    
+
+    //convertimos la lista de exprexiones para convertirla en lista de strings, es decir cada elemento se convertira en string
+    letrasPaDesencriptar = letrasParaDesencriptar.join(" ").replace(/[/g]/g, "").split(" ");
     //recorremos el texto 
     for(let i=0; i<texto.length; i++){
         //definimos una variable para guardar las letras que no son vocales
@@ -71,7 +34,7 @@ function encriptarFrase(texto){
             
             //si la letra coincide con una vocal dentro la lista, se hace el reemplazo por las letras a encriptar
             if(texto[i] === vocales[j] ){
-                textoEncriptado += letrasParaDesencriptar[j];
+                textoEncriptado += letrasPaDesencriptar[j];
             //si la letra no es una vocal que esta dentro la lista vocales, se guarda en la variable letra
             }else if(vocales.includes(texto[i]) == false){
                 letra = texto[i];
@@ -86,9 +49,11 @@ function encriptarFrase(texto){
 
 }
 
-function verificarCaracteres(texto, caracteresProhibidos){
+
+
+function verificarCaracteres(texto){
     //verificamos si existen caracteres especiales, devolvera false si no existen los caracteres
-    let existenCaracteres = (texto.match(caracteresNoPermitidos) != null)? [true, texto.match(caracteresProhibidos)] : false 
+    let existenCaracteres = (texto.match(caracteresNoPermitidos) != null)? [true, texto.match(caracteresNoPermitidos)] : false 
     return existenCaracteres;
 }
 function eliminarElemento(nombreClaseId){
@@ -101,37 +66,77 @@ function cambiarEstiloElemento(elemento,propiedad, valor){
     
     return;
 }
-function escribirTexto(elemento, texto){
+function asignarTexto(elemento, texto){
     document.querySelector(elemento).innerHTML = texto;
     return;
 }
 
+function ventanitaEmergente(mensaje, duracionSegundos, color){
+
+    
+    let contador = 0;
+    let tiempoSegundos = duracionSegundos;
+
+    cambiarEstiloElemento(".ventana__temporal", "display", "flex");
+    cambiarEstiloElemento(".ventana__temporal", "backgroundColor", color);
+    asignarTexto(".ventana__temporal__texto", mensaje);
+    let tiempo = setInterval(()=>{
+        contador ++;
+        if(contador == tiempoSegundos){
+            cambiarEstiloElemento(".ventana__temporal", "display", "none");
+            
+            clearInterval(tiempo);
+        }
+    }, 1000)
+
+}
+
+function animarTexto(elemento,texto, tiempo, color){
+
+    let cont = 0;
+    let text = document.querySelector(elemento);
+    
+    
+    let interv = setInterval(()=>{
+        if(cont == tiempo){
+            clearInterval(interv);
+        }
+    }, 1000)
+}
+
+
+
+
 function encriptar(){
     let text = document.getElementById("texto").value;
-    let caracteresEspeciales = verificarCaracteres(text, caracteresNoPermitidos);
+    let caracteresEspeciales = verificarCaracteres(text);
     let textoEncriptado = "";
 
     if(text != ""){
         console.log(text);
         if(caracteresEspeciales == false){
 
-            textoEncriptado = encriptarFrase(text);
+            textoEncriptado = encriptarTexto(text);
             if(primeraEncriptacion == true){
                 eliminarElemento(".encriptador__resultado__imagen");
                 eliminarElemento(".encriptador__resultado__texto");
                 cambiarEstiloElemento(".encriptador__resultado__texto__encriptado", "display", "flex");
-                escribirTexto("#textoEncriptado", textoEncriptado);
+                asignarTexto("#textoEncriptado", textoEncriptado);
                 primeraEncriptacion = false;
+                ventanitaEmergente("Texto encriptado con exito 😈", 2, colorAcertar);
             }else{
 
-                escribirTexto("#textoEncriptado", textoEncriptado);
+                asignarTexto("#textoEncriptado", textoEncriptado);
+                ventanitaEmergente("Texto encriptado con exito 😈", 2, colorAcertar);
             }
         }else{
             let caracterNoPermitido = caracteresEspeciales[1][0];
-            alert(`Ups parece que estas tratando de encriptar una letra no permitida : ${caracterNoPermitido}`)
+            ventanitaEmergente(`😿Parece que estas tratando de encriptar una letra no permitida:👉 ${caracterNoPermitido}`, 4, colorError);
+            //alert(`Ups parece que estas tratando de encriptar una letra no permitida : ${caracterNoPermitido}`)
         }
     }else{
-        alert("el campo esta vacio");
+        ventanitaEmergente("El campo esta vacio", 2, colorError);
+        
     }
     
 }
@@ -146,33 +151,35 @@ function desencriptar(){
     let textoDesencriptado = "";
 
     if(text != ""){
-        texto = text.split(" ");
-        /*for(let i=0; i<texto.length; i++){
-            textoDesencriptado += desencriptarPalabra(texto[i])+" ";
         
-        }*/
-        
-       textoDesencriptado = desencriptarPalabra(text);
+       textoDesencriptado = desencriptarTexto(text);
         if(primeraEncriptacion == true){
             eliminarElemento(".encriptador__resultado__imagen");
             eliminarElemento(".encriptador__resultado__texto");
             cambiarEstiloElemento(".encriptador__resultado__texto__encriptado", "display", "flex");
-            escribirTexto("#textoEncriptado", textoDesencriptado);
+            asignarTexto("#textoEncriptado", textoDesencriptado);
             primeraEncriptacion = false
+            ventanitaEmergente("Texto desencriptado con exito", 2, colorAcertar);
         }else{
-            escribirTexto("#textoEncriptado", textoDesencriptado);
+
+            asignarTexto("#textoEncriptado", textoDesencriptado);
+            ventanitaEmergente("Texto desencriptado con exito", 2, colorAcertar);
         }
         
     }else{
-        alert("el campo esta vacio");
+        ventanitaEmergente("El campo esta vacio", 2, colorError);
+        //alert("el campo esta vacio");
     }
 }
 
 function copiar(){
     let text = document.getElementById("textoEncriptado").innerText;
     
-    console.log(text);
     navigator.clipboard.writeText(text);
-    
+    ventanitaEmergente("Texto copiado", 1, colorAcertar);
+    let placeHold = document.getElementById("texto")
+    placeHold.value = "";
+    placeHold.placeholder="Ingrese el texto aqui";
 }
+
 
